9c1f9e37f24e7b7047cb27e83ececd4216251c39
QasimVibes <qasimrazzaq921@gmail.com>
2023-12-23 20:57:52 +0500

    Changes 0.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>Home</h1>
-  )
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold mb-4">Home</h1>
+      <ol className="list-decimal pl-4">
+        <li className="mb-2">
+          <Link href="/contact" className="text-blue-500 hover:underline">
+            Contact
+          </Link>
+        </li>
+      </ol>
+    </div>
+  );
 }
+
